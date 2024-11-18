6381c2f381896e5c189fd1c7212c31ec910e3825
mariomoreirasilva <mario@desban.org.br>
2024-11-18 15:52:00 -0300

    Tratamento erros completo, get department

--- src/main/java/com/devsuperior/workshopcassandra/controllers/DepartmentController.java
+++ src/main/java/com/devsuperior/workshopcassandra/controllers/DepartmentController.java
@@ -1,10 +1,12 @@
 package com.devsuperior.workshopcassandra.controllers;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +24,14 @@ public class DepartmentController {
 		
 		List<DepartmentDTO> list = service.findAll();
 		return ResponseEntity.ok(list);
+		
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<DepartmentDTO> findById(@PathVariable UUID id){
+		
+		DepartmentDTO obj = service.findById(id);
+		return ResponseEntity.ok(obj);
 		
 	}
 	
--- src/main/java/com/devsuperior/workshopcassandra/services/DepartmentService.java
+++ src/main/java/com/devsuperior/workshopcassandra/services/DepartmentService.java
@@ -1,7 +1,8 @@
 package com.devsuperior.workshopcassandra.services;
 
 import java.util.List;
-import java.util.stream.Collector;
+import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 import com.devsuperior.workshopcassandra.model.dto.DepartmentDTO;
 import com.devsuperior.workshopcassandra.model.entities.Department;
 import com.devsuperior.workshopcassandra.repositories.DepartmentRepository;
+import com.devsuperior.workshopcassandra.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class DepartmentService {
@@ -20,6 +22,14 @@ public class DepartmentService {
 		
 		List<Department> list = repository.findAll();
 		return list.stream().map(x -> new DepartmentDTO(x)).collect(Collectors.toList());
+	}
+	
+	public DepartmentDTO findById(UUID id) {
+		
+		Optional<Department> resultado = repository.findById(id);
+		Department entidade = resultado.orElseThrow(() -> new ResourceNotFoundException("ID não encontrado"));
+		return new DepartmentDTO(entidade);
+		
 	}
 	
 }
