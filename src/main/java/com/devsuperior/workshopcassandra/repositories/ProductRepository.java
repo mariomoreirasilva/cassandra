b4e3e8562db77df4797bfcab2992d990c9b559f4
mariomoreirasilva <mario@desban.org.br>
2024-11-25 11:52:55 -0300

    Busca por nome de departamento

--- src/main/java/com/devsuperior/workshopcassandra/repositories/ProductRepository.java
+++ src/main/java/com/devsuperior/workshopcassandra/repositories/ProductRepository.java
@@ -2,10 +2,16 @@ package com.devsuperior.workshopcassandra.repositories;
 
 import java.util.UUID;
 
+import org.springframework.data.cassandra.repository.AllowFiltering;
 import org.springframework.data.cassandra.repository.CassandraRepository;
 
 import com.devsuperior.workshopcassandra.model.entities.Product;
+import java.util.List;
+
 
 public interface ProductRepository extends CassandraRepository<Product, UUID>{
+	
+	@AllowFiltering
+	List<Product> findByDepartment(String department);
 
 }
---
+++ src/main/java/com/devsuperior/workshopcassandra/services/ProductService.java
@@ -0,0 +1,45 @@
+package com.devsuperior.workshopcassandra.services;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.workshopcassandra.model.dto.ProductDTO;
+import com.devsuperior.workshopcassandra.model.entities.Product;
+import com.devsuperior.workshopcassandra.repositories.ProductRepository;
+import com.devsuperior.workshopcassandra.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class ProductService {
+	@Autowired
+	private ProductRepository repository;
+	
+
+	
+	private Product buscaPorId(UUID id) {
+		Optional<Product> resultado = repository.findById(id);
+		return resultado.orElseThrow(() -> new ResourceNotFoundException("ID não encontrado"));
+		
+	}
+	
+	public ProductDTO findById(UUID id) {
+		
+		//Optional<Product> resultado = repository.findById(id);
+		Product entidade = buscaPorId(id);
+		return new ProductDTO(entidade);
+		
+	}
+	
+	public List<ProductDTO> ProcuraPorDepartmnet(String departamento){
+		
+		List<Product> list = repository.findByDepartment(departamento);
+		return list.stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());
+	}
+	
+	
+	
+}
